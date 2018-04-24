f285c8529748c648287f3e04e60dcfebcae3e392
code3338 <code3338@gmail.com>
2018-04-24 09:34:46 -0500

    Commit 3

--- project2API.js
+++ project2API.js
@@ -16,15 +16,14 @@ let cityInput = document.getElementById("cityInput");
 let stateInput = document.getElementById("stateInput");
 let stateContainer = document.getElementById("stateContainer");
 let yesSunset = document.getElementById("yesSunset");
+let noSunset = document.getElementById("noSunset");
 let sunsetYesNoContainer = document.getElementById("sunsetYesNoContainer");
 let btnReset = document.getElementById("btnReset");
 let commentPara = document.getElementById("commentPara");
 
 /*Setting an EventListener to radio button yes, that when the "yes" radio button is selected, the button name changes.*/
 yesSunset.addEventListener("click", function() {
-  if(yesSunset.checked == true) {
-    btn.innerHTML = "See time of sunrise and sunset";
-  }
+  btn.innerHTML = "See time of sunrise and sunset";
 });
 /*Setting an EventListener to radio button no, that when the "no" radio button is selected, the button name changes.*/
 noSunset.addEventListener("click", function() {
@@ -32,7 +31,6 @@ noSunset.addEventListener("click", function() {
 })
 
 /*Set addEventListener, so that when click button to see sunrise or sunrise and sunset times, the function declaration "load" is called. */
-btn.addEventListener("click", load);
 
 /*Set a function declaration called load. In this function we target the onreadystatechange method found in the new object we created called ajaxRequest. Within this method, we check what the readyState property value is, and based on this value, determines whether we have received the file or not.We also log out the status to confirm the file was successfully loaded, and the data is ready to use. */
 function load() {
@@ -46,7 +44,7 @@ function load() {
     else if(ajaxRequest.readyState == 3) {
       console.log("response received");
     }
-    else if(ajaxRequest.readyState == 4) {
+    else if(ajaxRequest.readyState == 4 && ajaxRequest.status == 200) {
       console.log("data received");
       console.log(ajaxRequest.status);
       let data = JSON.parse(ajaxRequest.response);
@@ -54,17 +52,15 @@ function load() {
       sunrisePara.innerHTML = ""; /*Set paragraph in HTML set equal to an empty string, so that our answer does not stack on top of each other. We want only one anser to display for time of sunrise.*/
       sunrisePara.appendChild(sunriseText);
 
-      /*Next, we add an event listener: When a user clicks the "see sunrise time" or "see sunrise and sunset times" button, if the yes sunset radio button is selected, we display the sunset time; else, we do not display the sunset time, and only display the sunrise time.*/
-      btn.addEventListener("click", function() {
-        if(yesSunset.checked == true) {
-          let sunsetText = document.createTextNode(data.results.sunset);
-          sunsetPara.innerHTML = "";
-          sunsetPara.appendChild(sunsetText);
-        }
-        else if (yesSunset.checked == false) {
-          sunsetPara.innerHTML= "";
-        }
-      });
+      /*Next, we add an if statement. When a user clicks the "see sunrise time" or "see sunrise and sunset times" button, if the yes sunset radio button is selected, we display the sunset time; else, we do not display the sunset time, and only display the sunrise time.*/
+      if(yesSunset.checked == true) {
+        let sunsetText = document.createTextNode(data.results.sunset);
+        sunsetPara.innerHTML = "";
+        sunsetPara.appendChild(sunsetText);
+      }
+      else if(yesSunset.checked == false) {
+        sunsetPara.innerHTML = "";
+      }
     }
     else {
       console.log("oops! Something went wrong");
@@ -72,13 +68,12 @@ function load() {
   }
   ajaxRequest.open("GET", "https://api.sunrise-sunset.org/json?lat=" + latPopulate.value + "&lng=" + lngPopulate.value + "&date=" + date.value);
   ajaxRequest.send();
-  console.log(date.value);
 }
 
 /*We have created a second AJAX call, where when the user enters a city and state, the latitude and longitude autopopulate.*/
 let locAjaxRequest = new XMLHttpRequest();
 
-stateInput.addEventListener("blur", locload);
+btn.addEventListener("click", locload);
 
 function locload() {
   locAjaxRequest.onreadystatechange = function() {
@@ -95,32 +90,36 @@ function locload() {
       console.log("data received");
       console.log(locAjaxRequest.status);
       let locdata = JSON.parse(locAjaxRequest.response);
-      console.log(locdata); /*Get rid of*/
-      let latPara = document.createElement("p");
-      let lngPara = document.createElement("p");
-      let latText = document.createTextNode(locdata.results[0].geometry.location.lat);
-      let lngText = document.createTextNode(locdata.results[0].geometry.location.lng);
 
-      latPopulate.value = latText.nodeValue;
-      lngPopulate.value = lngText.nodeValue;
+      /*if statement: if City does not exist, an alert will popup */
+      if (locdata.status == "ZERO_RESULTS") {
+        cityInput.innerHTML = "";
+        cityInput.readOnly = false;
+        cityInput.style.cursor = "text";
+        cityInput.style.backgroundColor = "white";
+        stateInput.disabled = false;
+        stateInput.innerHTML = "";
+        stateInput.style.backgroundColor = "white";
+        btn.innerHTML = "See time of sunrise";
+        sunrisePara.innerHTML = ""
+        sunsetPara.innerHTML = ""
+        commentPara.innerHTML = "";
+        commentPara.style.borderStyle = "none";
+        stateInput.style.cursor = "pointer";
+        alert('City not found. Check spelling and try again!');
+      };
 
-      /*This event listener grays out city and state input elements after the user clicks on the button.*/
-      btn.addEventListener("click", function() {
-        cityInput.setAttribute("readonly", true);
-        cityInput.style.backgroundColor = "#989898";
-        stateInput.setAttribute("disabled", true);
-        commentPara.innerHTML = 'Select a new date for your chosen City and State, and then reclick the "See time" button below, or select the reset button to start over.'
-      });
+      latPopulate.value = locdata.results[0].geometry.location.lat;
+      lngPopulate.value = locdata.results[0].geometry.location.lng;
 
-      /*This event listener returns input elements to their page load and reload formats when the reset button is selected.*/
-      btnReset.addEventListener("click",function() {
-         cityInput.readOnly = false;
-         cityInput.style.backgroundColor = "white";
-         stateInput.disabled = false;
-         stateInput.style.backgroundColor = "white";
-         btn.innerHTML = "See time of sunrise";
-         commentPara.innerHTML = "";
-     });
+      lngPopulate.innerHTML = latPopulate.value;
+      latPopulate.innerHTML = latPopulate.value;
+
+      /*Side note:if set equal to a textNode, can log out the value of the textNode using .nodeValue*/
+
+      /*We call the function load() after locload() fires, upon clicking the btn, so that we can get the latitude and longitude to use in the load() ajaxRequest. */
+      load();
+
    }
     else {
       console.log("oops! Something went wrong");
@@ -129,3 +128,30 @@ function locload() {
   locAjaxRequest.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?address=" + cityInput.value + "," + stateInput.value + "&key=AIzaSyAoZUVaz-SRIoOYulECllMb9Yx4M4xjFHI");
   locAjaxRequest.send();
 }
+
+/*This event listener grays out city and state input elements after the user clicks on the button.*/
+btn.addEventListener("click", function() {
+  cityInput.setAttribute("readonly", true);
+  cityInput.style.backgroundColor = "#989898";
+  stateInput.setAttribute("disabled", true);
+  commentPara.innerHTML = 'Select a new date for your chosen City and State, and then reclick the "See time" button below, or select the reset button to start over.'
+  commentPara.style.border = "solid 2px purple";
+  cityInput.style.cursor = "not-allowed";
+  stateInput.style.cursor = "not-allowed";
+});
+
+/*This event listener returns input elements to their page load and reload formats when the reset button is selected.*/
+btnReset.addEventListener("click", function() {
+   cityInput.readOnly = false;
+   cityInput.style.backgroundColor = "white";
+   stateInput.disabled = false;
+   stateInput.style.backgroundColor = "white";
+   btn.innerHTML = "See time of sunrise";
+   sunrisePara.innerHTML = ""
+   sunsetPara.innerHTML = ""
+   commentPara.innerHTML = "";
+   commentPara.style.borderStyle = "none";
+   cityInput.style.cursor = "grab";
+   stateInput.style.cursor = "pointer";
+
+});
